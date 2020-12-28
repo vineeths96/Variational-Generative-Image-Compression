3ae43e989fa2594e822577cfc7ddc0fc0afb585b
vineeths96 <vinsree1996@gmail.com>
2020-12-28 17:38:26 +0530

    Add train test code

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import torch
+import argparse
+from download_data import downloadData
+from VAE_GAN import train
+from validate_models import validate_models
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+def main():
+    downloadData()
+
+    arg_parser = argparse.ArgumentParser(description="Train model or test model (default)")
+    arg_parser.add_argument("--train-model", action="store_true", default=False)
+
+    argObj = arg_parser.parse_args()
+
+    if argObj.train_model:
+        encoder, generator = train()
+        torch.save(encoder.state_dict(), f"../models/encoder_{NUM_CHANNELS}.model")
+        torch.save(generator.state_dict(), f"../models/generator_{NUM_CHANNELS}.model")
+    else:
+        validate_models([2, 4, 8, 16, 28])
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/validate_models.py
@@ -0,0 +1,30 @@
+import torch
+from models import Encoder, Generator
+from data_loader import test_dataloader
+from utils import plot_image_grid
+from parameters import *
+
+
+def validate_models(channels):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    test_loader = test_dataloader()
+    test_batch = next(iter(test_loader)).to(device)
+
+    reconstructed_images = {}
+
+    for channel in channels:
+        NUM_CHANNELS = channel
+        encoder = Encoder(NUM_CHANNELS).to(device)
+        generator = Generator(NUM_CHANNELS).to(device)
+
+        encoder.load_state_dict(torch.load(f'../models/encoder_{NUM_CHANNELS}.model', map_location=torch.device('cpu')))
+        generator.load_state_dict(torch.load(f'../models/generator_{NUM_CHANNELS}.model', map_location=torch.device('cpu')))
+
+        encoder.eval()
+        generator.eval()
+
+        reconstructed_image = generator(encoder(test_batch))
+        reconstructed_images[NUM_CHANNELS] = reconstructed_image
+
+    plot_image_grid(test_batch, reconstructed_images, NUM_IMAGES_GRID)
